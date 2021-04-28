d8eb182ccf9204b2cf4cba6d3357d3e4d6324c69
v.volodin <v.volodin@corp.mail.ru>
2021-04-28 21:58:27 +0300

    init commit

--- main.py
+++ main.py
@@ -63,8 +63,7 @@ def main():
     # Start the Bot
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
-                          url_path=TOKEN)
-    updater.bot.setWebhook('https://tg-test-bot-volodin.herokuapp.com/' + TOKEN)
+                          url_path=TOKEN, webhook_url='https://tg-test-bot-volodin.herokuapp.com/')
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
