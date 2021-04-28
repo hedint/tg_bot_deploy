560386853d8a94e0d2659ceacb1c5b17c81e80dd
v.volodin <v.volodin@corp.mail.ru>
2021-04-28 22:35:46 +0300

    fix

--- main.py
+++ main.py
@@ -1,42 +1,75 @@
-import logging
-import os
+"""
+Simple Bot to reply to Telegram messages taken from the python-telegram-bot examples.
+Deployed using heroku.
+Author: liuhh02 https://medium.com/@liuhh02
+"""
+
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import os
 
+PORT = int(os.environ.get('PORT', 5000))
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+TOKEN = '1752966025:AAH4HsOL_7g7xRb8efZTeDNKBp6LyeT0fx4'
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
 def start(update, context):
-    update.effective_message.reply_text("Hi!")
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
+
+
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
 
 
 def echo(update, context):
-    update.effective_message.reply_text(update.effective_message.text)
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
 
 
-if __name__ == "__main__":
-    # Set these variable to the appropriate values
-    TOKEN = "1752966025:AAH4HsOL_7g7xRb8efZTeDNKBp6LyeT0fx4"
-    NAME = "tg-test-bot-volodin"
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
 
-    # Port is given by Heroku
-    PORT = os.environ.get('PORT', 80)
 
-    # Enable logging
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                        level=logging.INFO)
-    logger = logging.getLogger(__name__)
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater(TOKEN, use_context=True)
 
-    # Set up the Updater
-    updater = Updater(TOKEN)
+    # Get the dispatcher to register handlers
     dp = updater.dispatcher
-    # Add handlers
-    dp.add_handler(CommandHandler('start', start))
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
 
-    # Start the webhook
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
-                          url_path=TOKEN,
-                          webhook_url=f"https://{NAME}.herokuapp.com/{TOKEN}")
-    logger.error(f"https://{NAME}.herokuapp.com/{TOKEN}")
-    logger.error(f"https://{NAME}.herokuapp.com/{PORT}")
+                          url_path=TOKEN, webhook_url='https://tg-test-bot-volodin.herokuapp.com/' + TOKEN)
 
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
+
+
+if __name__ == '__main__':
+    main()
