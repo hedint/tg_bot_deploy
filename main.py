92805a81f97595a046ca109e7de4a4ebc0dfd907
v.volodin <v.volodin@corp.mail.ru>
2021-04-28 22:14:45 +0300

    fix

--- main.py
+++ main.py
@@ -1,64 +1,40 @@
-"""
-Simple Bot to reply to Telegram messages taken from the python-telegram-bot examples.
-Deployed using heroku.
-Author: liuhh02 https://medium.com/@liuhh02
-"""
-
 import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import os
 
-PORT = int(os.environ.get('PORT', 5000))
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
 
-logger = logging.getLogger(__name__)
-TOKEN = '1752966025:AAH4HsOL_7g7xRb8efZTeDNKBp6LyeT0fx4'
+def start(update, context):
+    update.effective_message.reply_text("Hi!")
 
-logger.error(msg="BLABLABLA")
 
-# Определяем функцию-обработчик сообщений.
-# У неё два параметра, сам бот и класс updater, принявший сообщение.
 def echo(update, context):
-    # У объекта класса Updater есть поле message,
-    # являющееся объектом сообщения.
-    # У message есть поле text, содержащее текст полученного сообщения,
-    # а также метод reply_text(str),
-    # отсылающий ответ пользователю, от которого получено сообщение.
-    update.message.reply_text(f"Я получил сообщение {update.message.text}")
+    update.effective_message.reply_text(update.effective_message.text)
 
 
-def main():
-    # Создаём объект updater.
-    # Вместо слова "TOKEN" надо разместить полученный от @BotFather токен
-    updater = Updater(os.getenv("TOKEN"), use_context=True)
+if __name__ == "__main__":
+    # Set these variable to the appropriate values
+    TOKEN = "1752966025:AAH4HsOL_7g7xRb8efZTeDNKBp6LyeT0fx4"
+    NAME = "tg-test-bot-volodin"
 
-    # Получаем из него диспетчер сообщений.
+    # Port is given by Heroku
+    PORT = os.environ.get('PORT')
+
+    # Enable logging
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
+    logger = logging.getLogger(__name__)
+
+    # Set up the Updater
+    updater = Updater(TOKEN)
     dp = updater.dispatcher
+    # Add handlers
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
 
-    # Создаём обработчик сообщений типа Filters.text
-    # из описанной выше функции echo()
-    # После регистрации обработчика в диспетчере
-    # эта функция будет вызываться при получении сообщения
-    # с типом "текст", т. е. текстовых сообщений.
-    text_handler = MessageHandler(Filters.text, echo)
-
-    # Зарегистрируем их в диспетчере.
-    dp.add_handler(text_handler)
-
-    # Запускаем цикл приема и обработки сообщений.
-    # Start the Bot
-    updater.start_polling()
-
-
-    # Ждём завершения приложения.
-    # (например, получения сигнала SIG_TERM при нажатии клавиш Ctrl+C)
+    # Start the webhook
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN,
+                          webhook_url=f"https://{NAME}.herokuapp.com/{TOKEN}")
     updater.idle()
-
-
-# Запускаем функцию main() в случае запуска скрипта.
-
-if __name__ == '__main__':
-    main()
