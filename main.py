6969d72d908f23172a4b6bd90d40af787fb2aff8
v.volodin <v.volodin@corp.mail.ru>
2021-04-28 21:33:52 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,51 @@
+# Импортируем необходимые классы.
+from dotenv import load_dotenv
+from telegram.ext import Updater, MessageHandler, Filters
+from telegram.ext import CallbackContext, CommandHandler
+import os
+
+load_dotenv()
+
+
+# Определяем функцию-обработчик сообщений.
+# У неё два параметра, сам бот и класс updater, принявший сообщение.
+def echo(update, context):
+    # У объекта класса Updater есть поле message,
+    # являющееся объектом сообщения.
+    # У message есть поле text, содержащее текст полученного сообщения,
+    # а также метод reply_text(str),
+    # отсылающий ответ пользователю, от которого получено сообщение.
+    update.message.reply_text(f"Я получил сообщение {update.message.text}")
+
+
+def main():
+    # Создаём объект updater.
+    # Вместо слова "TOKEN" надо разместить полученный от @BotFather токен
+    updater = Updater(os.getenv("TOKEN"), use_context=True)
+
+    # Получаем из него диспетчер сообщений.
+    dp = updater.dispatcher
+
+    # Создаём обработчик сообщений типа Filters.text
+    # из описанной выше функции echo()
+    # После регистрации обработчика в диспетчере
+    # эта функция будет вызываться при получении сообщения
+    # с типом "текст", т. е. текстовых сообщений.
+    text_handler = MessageHandler(Filters.text, echo)
+
+    # Зарегистрируем их в диспетчере.
+    dp.add_handler(text_handler)
+
+    # Запускаем цикл приема и обработки сообщений.
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(os.environ.get("PORT", 5000)),
+                          url_path=os.getenv("TOKEN"))
+
+    # Ждём завершения приложения.
+    # (например, получения сигнала SIG_TERM при нажатии клавиш Ctrl+C)
+    updater.idle()
+
+
+# Запускаем функцию main() в случае запуска скрипта.
+if __name__ == '__main__':
+    main()
